23477971f96a2d54c56e4782a135dfb07909003a
Chris Cox <ccox@cc.au.dk>
2020-09-22 16:57:12 +0200

    effect_size_calc

--- MA_code_audiovisual_congruence.R
+++ MA_code_audiovisual_congruence.R
@@ -1,24 +1,20 @@
 #Main q: are children regardless of anything sensitive to congruent audio-visual 
 #information? How big is the effect? Publication bias, etc.
 
-"Moderators:
-  
-- age
-- native vs. Non-native
-- stimuli type:
-  - exposure_phase
-- dependent measure
-- gender
-- ? gender of the voice
+#SD = squareroot(N) * (upperCI - lowerCI / 3.92), i.e 2 x 1.96 standard errors wide.
+#SE = SD / squareroot(n) ; SD = SE * squareroot(n)
 
-Varying effects
+#convert t-statistic to effect size:
+#for one-sample t:
+  #Cohen's d = t/Sqrt(n)
 
-- study_ID
-- exp_num inside study_ID
+library(tidyverse)
+library(dplyr)
 
-How to deal with effect size:
-  
-  - figure sd for baseline chance (formula)
-- figure out sd when it's missing in both
-- only t or a z: formula to calculate d (maybe se)"
-
+MA_data <- read.csv('/Users/au620441/Documents/GitHub/MyGitRepoCCox1/MA_audiovisual_congruence/MA_audiovisual_congruence/MA_Data_Audiovisual_Congruence3.csv')
+#use t-values to calculate d:
+MA_data <- as_tibble(MA_data)
+MA_data %>%
+  mutate(MA_data, cohen_d = t / sqrt(n_1)) %>%
+  select(t, cohen_d, everything()) %>%
+  head
