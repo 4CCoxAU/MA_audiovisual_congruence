310237db9a053310d453611d7fe1fe8b8d0dcd65
Chris Cox <ccox@cc.au.dk>
2020-09-22 14:47:31 +0200

    22-09-20_commit

---
+++ MA_code_audiovisual_congruence.R
@@ -0,0 +1,24 @@
+#Main q: are children regardless of anything sensitive to congruent audio-visual 
+#information? How big is the effect? Publication bias, etc.
+
+"Moderators:
+  
+- age
+- native vs. Non-native
+- stimuli type:
+  - exposure_phase
+- dependent measure
+- gender
+- ? gender of the voice
+
+Varying effects
+
+- study_ID
+- exp_num inside study_ID
+
+How to deal with effect size:
+  
+  - figure sd for baseline chance (formula)
+- figure out sd when it's missing in both
+- only t or a z: formula to calculate d (maybe se)"
+
