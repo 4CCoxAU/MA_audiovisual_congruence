753c70736755c86095e744df9d8e7c8d8c6225bb
Chris Cox <ccox@cc.au.dk>
2020-09-23 08:59:32 +0200

    cohen_d_calc

--- MA_code_audiovisual_congruence.R
+++ MA_code_audiovisual_congruence.R
@@ -12,23 +12,21 @@ library('tidyverse')
 library('dplyr')
 
 #import data
-MA_data <- read.csv('/Users/au620441/Documents/GitHub/MyGitRepoCCox1/MA_audiovisual_congruence/MA_audiovisual_congruence/MA_Data_Audiovisual_Congruence3.csv')
+MA_data <- read.csv('/Users/au620441/Documents/GitHub/MyGitRepoCCox1/MA_audiovisual_congruence/MA_audiovisual_congruence/MA_Data_Audiovisual_Congruence4.csv')
 MA_data <- as_tibble(MA_data)
-
-#use t-values to calculate d:
-MA_data <- MA_data %>%
-  mutate(MA_data, cohen_d = t / sqrt(n_1)) %>%
-  relocate(cohen_d, .after = t)
 
 #calculate average SD_1 to use for missing values:
 MA_data %>%
   filter(SD_1 < 1) %>%
   summarise(mean(SD_1,na.rm=TRUE))
 
-#calculate average SD_2 (baseline condition) to use for missing values:
-MA_data %>%
-  filter(SD_2 < 1) %>%
-  summarise(mean(SD_2,na.rm=TRUE))
-    
+#use mean and sd to calculate d:
+MA_data <- MA_data %>%
+  mutate(MA_data, cohen_d = (x_1-x_2) / SD_1) %>%
+  relocate(cohen_d, .after = t)
 
+#use t-values to calculate d:
+MA_data <- MA_data %>%
+  mutate(MA_data, cohen_d2 = t / sqrt(n_1)) %>%
+  relocate(cohen_d2, .after = cohen_d)
 
